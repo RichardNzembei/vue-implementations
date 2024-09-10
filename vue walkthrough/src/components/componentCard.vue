8c08dd649c965764da84c3e2a47a94618762dcb6
RichardNzembei <richardsonreuben78@gmail.com>
2024-09-10 14:32:20 +0300

    vue concepts and illustrations

---
+++ vue walkthrough/src/components/componentCard.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {ref} from 'vue'
+const props=defineProps(['title','date','time','location','description'])
+</script>
+<template>
+    <div class="blogposts">
+        
+        <h4>{{ title }}</h4>
+        <span>{{ date }}</span>
+        <strong>{{ time }}</strong><br>
+        <span>{{ location }}</span>
+        <p>{{ description }}</p>
+        <button>join</button>
+
+    </div>
+    
+</template>
+<style scoped>
+.blogposts{
+    border: 1px solid blue;
+    height: max-content;
+    margin: 10px;
+    padding: 10px;
+    border-radius: 10px;
+    
+}
+h4{
+    color: aqua;
+}
+
+</style>
---
+++ vue walkthrough/src/components/productCard.vue
@@ -0,0 +1,9 @@
+<script setup>
+const message=''
+</script>
+<template>
+    <input type="text" v-model.lazy="message">
+    <p>message is:{{ message }}</p>
+</template>
+<style>
+</style>
