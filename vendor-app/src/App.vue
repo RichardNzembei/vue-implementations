854078330d5f35fdc26b7c22caa4761a37bd29aa
RichardNzembei <richardsonreuben78@gmail.com>
2024-09-12 21:36:26 +0300

    vendoapp first commit

---
+++ vendor-app/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {ref} from 'vue'
+import homeCard from './components/homeCard.vue';
+import contactCard from './components/contactCard.vue';
+import productCard from './components/productCard.vue';
+
+</script>
+<template>
+    <nav>
+      <a href="#home">Home</a>
+      <a href="#product">Product</a>
+      <a href="#contact">Contact</a>
+    </nav>
+</template>
+<style scoped>
+
+</style>
