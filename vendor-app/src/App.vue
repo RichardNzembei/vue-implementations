b844359947da347e41b9fc508bd19f470edbc7c4
RichardNzembei <richardsonreuben78@gmail.com>
2024-09-13 14:00:32 +0300

    code format

--- vendor-app/src/App.vue
+++ vendor-app/src/App.vue
@@ -1,71 +1,65 @@
 <script setup>
-import {onMounted, ref} from 'vue'
-import homeCard from './components/homeCard.vue';
-import contactCard from './components/contactCard.vue';
-import productCard from './components/productCard.vue';
-const currentView=ref(homeCard)
-function onHashChange(){
-  const hash=window.location.hash.substring(1)
-  if(hash==='product'){
-    currentView.value=productCard
-  }else if(hash==='contact'){
-    currentView.value=contactCard
-  }
-  else{
-    currentView.value=homeCard
+import { onMounted, ref } from "vue";
+import homeCard from "./components/homeCard.vue";
+import contactCard from "./components/contactCard.vue";
+import productCard from "./components/productCard.vue";
+const currentView = ref(homeCard);
+function onHashChange() {
+  const hash = window.location.hash.substring(1);
+  if (hash === "product") {
+    currentView.value = productCard;
+  } else if (hash === "contact") {
+    currentView.value = contactCard;
+  } else {
+    currentView.value = homeCard;
   }
 }
-onMounted(()=>{
-  window.addEventListener('hashchange', onHashChange)
-  onHashChange()
-
-})
-
+onMounted(() => {
+  window.addEventListener("hashchange", onHashChange);
+  onHashChange();
+});
 </script>
 <template>
-    <nav class="navbar">
-      <a class="items" href="#home">Home</a>
-      <a class="items" href="#product">Product</a>
-      <a class="items" href="#contact">Contact</a>
-      <div class="logo">
-                
-                <span class=" icon icon-farm"></span>
-            </div>  
-    </nav>
-    <component :is="currentView"/>
+  <nav class="navbar">
+    <a class="items" href="#home">Home</a>
+    <a class="items" href="#product">Product</a>
+    <a class="items" href="#contact">Contact</a>
+    <div class="logo">
+      <span class="icon icon-farm"></span>
+    </div>
+  </nav>
+  <component :is="currentView" />
 </template>
 <style scoped>
-.navbar{
-    display:flex;
-    justify-content:space-around;
-    align-items: center;
-    padding: 14px;
-    
-    
-    background-color: rgb(143, 172, 162);
-    
+.navbar {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 14px;
+
+  background-color: rgb(143, 172, 162);
 }
-.navbar.logo{
-    display: flex;
-    align-items: center;
-    position: relative;
+.navbar.logo {
+  display: flex;
+  align-items: center;
+  position: relative;
 }
 
-.nav-links{
-    list-style: none;
-    display: flex;
+.nav-links {
+  list-style: none;
+  display: flex;
 }
-.nav-items{
-    margin: 0 15px;
+.nav-items {
+  margin: 0 15px;
 }
-.nav-items a{
-    color: white;
-    text-decoration: none;
-    font-size: 1.1em;
+.nav-items a {
+  color: white;
+  text-decoration: none;
+  font-size: 1.1em;
 }
-.nav-items a:hover{
-    color: #ddd;
-} 
+.nav-items a:hover {
+  color: #ddd;
+}
 .icon {
   width: 32px;
   height: 32px;
@@ -73,10 +67,10 @@ onMounted(()=>{
   background-size: cover;
   display: inline-block;
 }
-.icon-farm{
-    background-image: url('./assets/images/farm.png');
-    font-size: 24px;
-    border-radius: 50%;
-    position: relative;
+.icon-farm {
+  background-image: url("./assets/images/farm.png");
+  font-size: 24px;
+  border-radius: 50%;
+  position: relative;
 }
 </style>
--- vendor-app/src/components/productCard.vue
+++ vendor-app/src/components/productCard.vue
@@ -1,30 +1,27 @@
 <script setup>
-
 </script>
 <template>
-      <div class="search">
-            <input type="text" v-model="searchTerm" placeholder="Search by category">
-            <button @click="searchProducts"><i class="fa fa-search"></i></button>
-        </div>
+  <div class="search">
+    <input type="text" v-model="searchTerm" placeholder="Search by category" />
+    <button @click="searchProducts"><i class="fa fa-search"></i></button>
+  </div>
 
-        <div class="card-container">
-            <div class="card" v-for="product in filteredProducts" :key="product.id">
-                <span :class="product.icon" class="icon"></span>
-                <h3>{{ product.name }}</h3>
-                <h4>Price: Ksh {{ product.price }}</h4>
-                <div class="row">
-                    <div class="cell">
-                      <label><b>Quantity:</b></label>
-                    </div>
-                    <div class="cell">
-                      <input type="number" v-model.number="product.quantity">
-                    </div>
-                </div>
-                <button @click="addToCart(product)" class="addToCart">Add to Cart</button>
-            </div>
+  <div class="card-container">
+    <div class="card" v-for="product in filteredProducts" :key="product.id">
+      <span :class="product.icon" class="icon"></span>
+      <h3>{{ product.name }}</h3>
+      <h4>Price: Ksh {{ product.price }}</h4>
+      <div class="row">
+        <div class="cell">
+          <label><b>Quantity:</b></label>
         </div>
-        
+        <div class="cell">
+          <input type="number" v-model.number="product.quantity" />
+        </div>
+      </div>
+      <button @click="addToCart(product)" class="addToCart">Add to Cart</button>
+    </div>
+  </div>
 </template>
 <style scoped>
-
 </style>
